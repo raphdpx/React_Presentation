4ab0b5912b17e08536cbf48b7f296005af4a4f1d
Raphael Dispaux <raphael.dispaux@nsi-sa.be>
2019-12-10 13:38:45 +0100

    finishing touch

--- React_Presentation/RP.Frontend/src/assets/index.ts
+++ React_Presentation/RP.Frontend/src/assets/index.ts
@@ -2,7 +2,7 @@ const assets = {
   images: {
     nsiLogo: require("./nsi-logo.png") as string,
     nsiLogoWhite: require("./nsi_logo_white.png") as string,
-    presentationLogo: require("./presentation_react_logo.jpg") as string
+    presentationLogo: require("./nsi-presentation-react-logo.png") as string
   }
 };
 
--- React_Presentation/RP.Frontend/src/pages/home/item/HomePage.tsx
+++ React_Presentation/RP.Frontend/src/pages/home/item/HomePage.tsx
@@ -1,15 +1,25 @@
 import * as React from "react";
-import { PageLayout } from "nsitools-react";
 import assets from "../../../assets";
+import styled from "styled-components";
 
 interface IHomePageProps {}
 
+const StyledPageLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  width: 100%;
+  height: 100%;
+  align-self: stretch;
+  overflow: auto;
+`;
+
 export const HomePage: React.FunctionComponent<IHomePageProps> = props => {
   return (
-    <PageLayout>
+    <StyledPageLayout>
       <div style={{ display: "flex", flex: 1, flexDirection: "column" }}>
         <img src={assets.images.presentationLogo} alt="Presentation React"></img>
       </div>
-    </PageLayout>
+    </StyledPageLayout>
   );
 };
